a0fb8ea2ca6429715ce5c2614949f83c89d2d62d
justinever9 <justinever9@gmail.com>
2022-05-04 01:34:04 +0700

    init sub categories page

--- parsers/categories.rb
+++ parsers/categories.rb
@@ -1,1 +1,20 @@
 body = JSON.parse(content)
+
+body['data']['categoryChildren'].each do |cat|
+    category = cat['name'].unicode_normalize(:nfkd).encode('ASCII', replace: '')
+    variables = JSON.parse('{"hideUnavailableItems":true,"skusFilter":"FIRST_AVAILABLE","simulationBehavior":"default","installmentCriteria":"MAX_WITHOUT_INTEREST","productOriginVtex":false,"map":"c,c","query":"electro/'+ category.gsub(',','').gsub(' ','-') +'","orderBy":"OrderByScoreDESC","from":0,"to":200,"selectedFacets":[{"key":"c","value":"electro"},{"key":"c","value":"'+ category.gsub(',','').gsub(' ','-') +'"}],"operator":"and","fuzzy":"0","searchState":null,"facetsBehavior":"Static","categoryTreeBehavior":"default","withFacets":false}')
+
+    encoded_variables = Base64.strict_encode64(JSON.generate(variables))
+    url = 'https://www.disco.com.ar/_v/segment/graphql/v1?workspace=master&maxAge=short&appsEtag=remove&domain=store&locale=es-AR&__bindingId=0beab475-23b8-4674-b38a-956cc988dade&operationName=productSearchV3&variables={}&extensions={"persistedQuery":{"version":1,"sha256Hash":"6869499be99f20964918e2fe0d1166fdf6c006b1766085db9e5a6bc7c4b957e5","sender":"vtex.store-resources@0.x","provider":"vtex.search-graphql@0.x"},"variables":"'+encoded_variables+'"}'
+
+    pages << {
+        url: url,
+        page_type: "listings",
+        http2: false,
+        method: "GET",
+        headers: page['headers'],
+        vars: {
+            url_variables: variables,
+        },
+    }
+end
