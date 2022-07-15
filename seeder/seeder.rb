b12e606ca1b6e36b77917836e80b39646907c88b
justinever9 <justinever9@gmail.com>
2022-07-15 21:25:13 +0700

    test query

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -20,7 +20,7 @@ id_list.each_with_index do |id, i|
             'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
         },
         vars: {
-            cat_name: cat_name[i]   
+            cat_name: cat_name[i]
         }
     }
 end
