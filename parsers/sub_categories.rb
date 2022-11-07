8d847076377aade2c524615078f7ae6d05fb8730
justinever9 <justinever9@gmail.com>
2022-11-07 20:48:13 +0700

    update http2

--- parsers/sub_categories.rb
+++ parsers/sub_categories.rb
@@ -87,7 +87,7 @@ products.each_with_index do |prod, i|
     pages << {
         url: url,
         page_type: "details",
-        http2: false,
+        http2: trueß,
         fetch_type: 'browser',
         method: "GET",
         # cookie: page['headers']["cookie"],
