4feafa72b8c9e59615c8848a4ed384bc40293a55
justinever9 <justinever9@gmail.com>
2022-11-16 20:03:15 +0700

    update header

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -43,8 +43,9 @@ pages << {
         'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-Encoding' => 'gzip, deflate, br',
         'Accept-Language' => 'en-US,en;q=0.9',
-        'Content-Type' => 'application/json',
-        'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        'service-worker-navigation-preload' => 'true',
+        'if-none-match' => "ECFCF3D4AF9A667FE0333AE5B7588AE7",
+        'User-Agent' => 'Mozilla/5.0 (Macitosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
     }
     # vars: {
     #     out: out,
