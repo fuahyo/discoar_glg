ccc0bfcdeef8798376d1b59d09422a301450e383
justinever9 <justinever9@gmail.com>
2023-03-22 21:37:39 +0700

    update sha hash

--- lib/sha256hash.rb
+++ lib/sha256hash.rb
@@ -5,7 +5,7 @@ class Sha256Hash
     def self.hash
         {
             "productSearchV3" => "8d81f8e7468bea2046b664a5a1d9e4bb4e1f3f9e340dcb3701fd68b15c0e8025",
-            "facetsV2" => "b48edf6aa0417d4229198e3eb79f3549eaa01be3bd1c86f4ea1daadefa9b38ba",
+            "facetsV2" => "d8e66172ecea20cc2ab0a0232f4b8c2ef6d2b7965d7a19785e9cb3799c1dd35c",
             "Product" => "d8e66172ecea20cc2ab0a0232f4b8c2ef6d2b7965d7a19785e9cb3799c1dd35c",
         }
     end
