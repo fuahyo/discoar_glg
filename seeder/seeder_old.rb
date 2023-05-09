6e7ed01249972b27f6f980b65e9a2581acb356b7
justinever9 <justinever9@gmail.com>
2023-05-10 00:23:35 +0700

    test till on seeder_old file

--- seeder/seeder_old.rb
+++ seeder/seeder_old.rb
@@ -25,26 +25,29 @@ cat_name = ['electro', 'bebidas', 'tiempo libre', 'almacen', 'carnes', 'frutas y
 #     }
 # end
 pages << {
-    url: "https://www.disco.com.ar/cafe-instantaneo-nescafe-gold-x-100-gr/p",
+    url: "https://www.disco.com.ar/molinillo-cafe-nex-cg1250bl/p",
     page_type: "details",
-    http2: false,
-    fetch_type: 'browser',
-    method: "GET",
-    driver: {
-        "code": "await sleep(10000);",
-        "goto_options": {
-            "timeout": 0,
-            "waitUntil": "domcontentloaded",
-        },
-    },
-    cookie: "_hjSessionUser_704952=eyJpZCI6ImFmZDEwNDlkLTQ5Y2QtNTljZS05NjM3LTE0NWQ3MDFiZTUzYiIsImNyZWF0ZWQiOjE2NTE4NDcxNjcwMTMsImV4aXN0aW5nIjp0cnVlfQ==; VtexWorkspace=master%3A-; _gcl_au=1.1.1416430178.1667544780; _fbp=fb.2.1667544780178.796434972; __kdtv=t%3D1651847166846%3Bi%3D529a8b804192573c1bd63411cd23152e63bf4f6a; _kdt=%7B%22t%22%3A1651847166846%2C%22i%22%3A%22529a8b804192573c1bd63411cd23152e63bf4f6a%22%7D; biggy-anonymous=u6O5OSJlKBYCB5uANA1oh; vtex_binding_address=discoargentina.myvtex.com/; VtexRCMacIdv7=b4edd7c1-e5a1-4c15-8442-8679868b7882; checkout.vtex.com=__ofid=e5a23430f43142888f719b49603c1a79; .ASPXAUTH=EB88FF2FF52334FAB14F5B405774C17135C48FC5EDE09C977AFB81A8611F503E97C4D19B0935BA019B09832E70505395096E4B4DEA49B9BD67545A926FFBEB6EAC6D0C8029468B18338882AEDCEF77F858BA9BA078B164014D4F802D1934F0AFAB4C2D0F0C08B9FA51308A7A9630F371A6B485EEC9A98AC41078C8D862FD220138BE60EBFD834ECF372825F012B1C4C385C016BCA6DDA3976E187535EC912C16527E13B5; vtex_session=eyJhbGciOiJFUzI1NiIsImtpZCI6IkZFRkU2QTg5QUExQTY2NjJDMURCQ0ZFMDcxRUFGMEE2MTgxMTI0MTUiLCJ0eXAiOiJqd3QifQ.eyJhY2NvdW50LmlkIjoiMWUyOTg4N2YtNGQ0My00ODRmLWI1MTItMjAxM2Y0MjYwMGIxIiwiaWQiOiIzMDQ3Y2NlMy1kZTdhLTQ3NWEtYjE1OC00ZDIyYTgwN2RkMTIiLCJ2ZXJzaW9uIjo1LCJzdWIiOiJzZXNzaW9uIiwiYWNjb3VudCI6InNlc3Npb24iLCJleHAiOjE2Njg1MTI0MzksImlhdCI6MTY2NzgyMTIzOSwiaXNzIjoidG9rZW4tZW1pdHRlciIsImp0aSI6ImYzN2VlMDA0LWNjMTEtNGU3MC04NjQ2LWVkMjY1MDIyMTEyYyJ9.Lxrz8qR9woxMYuNXDpPE3fHCBdLAIY5da7cGv9iuVd-4r_ApwXQHc5mqaX2pnwbS--nSrPlzvzq8HWV5NogdsA; vtex_segment=eyJjYW1wYWlnbnMiOm51bGwsImNoYW5uZWwiOiIzMyIsInByaWNlVGFibGVzIjpudWxsLCJyZWdpb25JZCI6IlUxY2phblZ0WW05aGNtZGxiblJwYm1Ga01ETTEiLCJ1dG1fY2FtcGFpZ24iOm51bGwsInV0bV9zb3VyY2UiOm51bGwsInV0bWlfY2FtcGFpZ24iOm51bGwsImN1cnJlbmN5Q29kZSI6IkFSUyIsImN1cnJlbmN5U3ltYm9sIjoiJCIsImNvdW50cnlDb2RlIjoiQVJHIiwiY3VsdHVyZUluZm8iOiJlcy1BUiIsImFkbWluX2N1bHR1cmVJbmZvIjoiZXMtQVIiLCJjaGFubmVsUHJpdmFjeSI6InB1YmxpYyJ9; _hjIncludedInSessionSample=1; _hjSession_704952=eyJpZCI6IjkyYjI5NTJhLTE1MTMtNDZiNC1iODYwLTBmYzY4NmVhNzUwMSIsImNyZWF0ZWQiOjE2NjgxNDc0NjE1NjgsImluU2FtcGxlIjp0cnVlfQ==; _hjIncludedInPageviewSample=1; _hjAbsoluteSessionInProgress=0; biggy-session-discoargentina=J5mqGPTgSAOzS5GivFJyu; _gid=GA1.3.601466735.1668147468; _ga=GA1.3.2084765671.1667544782; _ga_4YZ7VMXH3C=GS1.1.1668147468.8.1.1668147746.0.0.0; biggy-event-queue=",
+    # http2: false,
     headers: {
-        'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Accept-Encoding' => 'gzip, deflate, br',
-        'Accept-Language' => 'en-US,en;q=0.9',
-        'service-worker-navigation-preload' => 'true',
-        'if-none-match' => "ECFCF3D4AF9A667FE0333AE5B7588AE7",
-        'User-Agent' => 'Mozilla/5.0 (Macitosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        'X-DH-Session-ID' => "my_session"
     }
+    # fetch_type: 'browser',
+    method: "GET",
+    # driver: {
+    #     "code": "await sleep(10000);",
+    #     "goto_options": {
+    #         "timeout": 0,
+    #         "waitUntil": "domcontentloaded",
+    #     },
+    # },
+    # cookie: "_hjSessionUser_704952=eyJpZCI6ImFmZDEwNDlkLTQ5Y2QtNTljZS05NjM3LTE0NWQ3MDFiZTUzYiIsImNyZWF0ZWQiOjE2NTE4NDcxNjcwMTMsImV4aXN0aW5nIjp0cnVlfQ==; VtexWorkspace=master%3A-; _gcl_au=1.1.1416430178.1667544780; _fbp=fb.2.1667544780178.796434972; __kdtv=t%3D1651847166846%3Bi%3D529a8b804192573c1bd63411cd23152e63bf4f6a; _kdt=%7B%22t%22%3A1651847166846%2C%22i%22%3A%22529a8b804192573c1bd63411cd23152e63bf4f6a%22%7D; biggy-anonymous=u6O5OSJlKBYCB5uANA1oh; vtex_binding_address=discoargentina.myvtex.com/; VtexRCMacIdv7=b4edd7c1-e5a1-4c15-8442-8679868b7882; checkout.vtex.com=__ofid=e5a23430f43142888f719b49603c1a79; .ASPXAUTH=EB88FF2FF52334FAB14F5B405774C17135C48FC5EDE09C977AFB81A8611F503E97C4D19B0935BA019B09832E70505395096E4B4DEA49B9BD67545A926FFBEB6EAC6D0C8029468B18338882AEDCEF77F858BA9BA078B164014D4F802D1934F0AFAB4C2D0F0C08B9FA51308A7A9630F371A6B485EEC9A98AC41078C8D862FD220138BE60EBFD834ECF372825F012B1C4C385C016BCA6DDA3976E187535EC912C16527E13B5; vtex_session=eyJhbGciOiJFUzI1NiIsImtpZCI6IkZFRkU2QTg5QUExQTY2NjJDMURCQ0ZFMDcxRUFGMEE2MTgxMTI0MTUiLCJ0eXAiOiJqd3QifQ.eyJhY2NvdW50LmlkIjoiMWUyOTg4N2YtNGQ0My00ODRmLWI1MTItMjAxM2Y0MjYwMGIxIiwiaWQiOiIzMDQ3Y2NlMy1kZTdhLTQ3NWEtYjE1OC00ZDIyYTgwN2RkMTIiLCJ2ZXJzaW9uIjo1LCJzdWIiOiJzZXNzaW9uIiwiYWNjb3VudCI6InNlc3Npb24iLCJleHAiOjE2Njg1MTI0MzksImlhdCI6MTY2NzgyMTIzOSwiaXNzIjoidG9rZW4tZW1pdHRlciIsImp0aSI6ImYzN2VlMDA0LWNjMTEtNGU3MC04NjQ2LWVkMjY1MDIyMTEyYyJ9.Lxrz8qR9woxMYuNXDpPE3fHCBdLAIY5da7cGv9iuVd-4r_ApwXQHc5mqaX2pnwbS--nSrPlzvzq8HWV5NogdsA; vtex_segment=eyJjYW1wYWlnbnMiOm51bGwsImNoYW5uZWwiOiIzMyIsInByaWNlVGFibGVzIjpudWxsLCJyZWdpb25JZCI6IlUxY2phblZ0WW05aGNtZGxiblJwYm1Ga01ETTEiLCJ1dG1fY2FtcGFpZ24iOm51bGwsInV0bV9zb3VyY2UiOm51bGwsInV0bWlfY2FtcGFpZ24iOm51bGwsImN1cnJlbmN5Q29kZSI6IkFSUyIsImN1cnJlbmN5U3ltYm9sIjoiJCIsImNvdW50cnlDb2RlIjoiQVJHIiwiY3VsdHVyZUluZm8iOiJlcy1BUiIsImFkbWluX2N1bHR1cmVJbmZvIjoiZXMtQVIiLCJjaGFubmVsUHJpdmFjeSI6InB1YmxpYyJ9; _hjIncludedInSessionSample=1; _hjSession_704952=eyJpZCI6IjkyYjI5NTJhLTE1MTMtNDZiNC1iODYwLTBmYzY4NmVhNzUwMSIsImNyZWF0ZWQiOjE2NjgxNDc0NjE1NjgsImluU2FtcGxlIjp0cnVlfQ==; _hjIncludedInPageviewSample=1; _hjAbsoluteSessionInProgress=0; biggy-session-discoargentina=J5mqGPTgSAOzS5GivFJyu; _gid=GA1.3.601466735.1668147468; _ga=GA1.3.2084765671.1667544782; _ga_4YZ7VMXH3C=GS1.1.1668147468.8.1.1668147746.0.0.0; biggy-event-queue=",
+    # headers: {
+    #     'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    #     'Accept-Encoding' => 'gzip, deflate, br',
+    #     'Accept-Language' => 'en-US,en;q=0.9',
+    #     'service-worker-navigation-preload' => 'true',
+    #     'if-none-match' => "ECFCF3D4AF9A667FE0333AE5B7588AE7",
+    #     'User-Agent' => 'Mozilla/5.0 (Macitosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    # }
 
 }
