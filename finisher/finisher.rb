12f3b4ff1a90cb78e057269c29f3f5355d170ee5
justinever9 <justinever9@gmail.com>
2023-07-05 12:46:15 +0700

    change exporter name UY

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -12,12 +12,16 @@ while true
 #   require 'byebug'
 #   byebug
   records.each do |product|
+    product['country_iso'] = 'UY'
+    product['currency_code_lc'] = 'UYU'
+    product['store_name'] = 'Disco Uruguay'
+    product['language'] = 'SPA'
+    product['barcode'] = product['competitor_product_id']
     if product['page_number'].nil?
       product['page_number'] = 1
-      outputs << product
-
-      save_outputs(outputs) if outputs.count > 99
     end
+    outputs << product
+    save_outputs(outputs) if outputs.count > 99
   end
 
   break if records.nil? || records.count < 1
