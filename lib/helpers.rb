7cd84aebc0a307243a1b009ccded413fad7cc2e2
justinever9 <justinever9@gmail.com>
2023-07-11 19:01:58 +0700

    fix for AR country

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -21,7 +21,7 @@ module Helpers
     end
     
     def self.website
-        'https://www.disco.com.uy'
+        'https://www.disco.com.ar'
     end
 
 
--- parsers/main_page.rb
+++ parsers/main_page.rb
@@ -13,8 +13,7 @@ if !cookie || cookie.empty?
 end
 
 pages << {
-    # url: 'https://www.disco.com.uy/api/sessions?sc=33',
-    url: 'https://www.disco.com.uy/api/sessions',
+    url: 'https://www.disco.com.ar/api/sessions?sc=33',
     page_type: "set_location",
     headers: {
         'accept': '*/*',
@@ -22,13 +21,12 @@ pages << {
         'cache-control': 'no-cache',
         'cookie': cookie,
         'content-type': 'application/json',
-        'origin': 'https://www.disco.com.uy',
+        'origin': 'https://www.disco.com.ar',
         'pragma': 'no-cache',
-        'referer': 'https://www.disco.com.uy/',
+        'referer': 'https://www.disco.com.ar/',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52'
       },
-    # body: '{"public":{"buyselectMethod":{"value":"recogida"},"regionId":{"value":"U1cjanVtYm9hcmdlbnRpbmFkMDM1"}}}',
-    body: '{"public":{"country":{"value":"URY"},"geoCoordinates":{"value":"-56.18187519999999,-34.914079"}}}',
+    body: '{"public":{"buyselectMethod":{"value":"recogida"},"regionId":{"value":"U1cjanVtYm9hcmdlbnRpbmFkMDM1"}}}',
     method: "POST",
     http2: true,
     fetch_type: "standard",
