4d231b51901ad1aa3129612aab1ef97d053967e5
justinever9 <justinever9@gmail.com>
2023-09-19 19:33:25 +0700

    update sha256 hash

--- lib/sha256hash.rb
+++ lib/sha256hash.rb
@@ -4,8 +4,8 @@ class Sha256Hash
     # https://www.carulla.com/
     def self.hash
         {
-            "productSearchV3" => "97f345cd1295d67e2e7c6e46f67b7d4e4593b2f97c26b22b9e6b68f787eb12ac",
-            "facetsV2" => "0e2c17bcc215d6dfd84498821cc43785841f1a53ce9821ec0e57adf26f7cdb38",
+            "productSearchV3" => "5582a05af7ee7b8a43406c6041335d5e4f0a6eea4a16f4bef9d2599218766e82",
+            "facetsV2" => "15475d03f959eb4fb3fd6240d4fa7952455f6813d4d6a46a99a9482c8c9e972d",
             "Product" => "8255fe793a58cbaed1c190bf47f456f8a77478fed0f7bb17ce1139efc96ee574",
         }
     end
