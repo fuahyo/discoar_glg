3ea35373deda9001d8a5a0679961701de18a983b
justinever9 <justinever9@gmail.com>
2024-03-19 18:56:03 +0700

    update main_page

--- parsers/main_page.rb
+++ parsers/main_page.rb
@@ -26,7 +26,7 @@ pages << {
         'referer': 'https://www.disco.com.ar/',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52'
       },
-    body: '{"public":{"buyselectMethod":{"value":"recogida"},"regionId":{"value":"U1cjanVtYm9hcmdlbnRpbmFkMDM1"}}}',
+    body: '{"public":{"country":{"value":"ARG"},"buyselectMethod":{"value":"delivery"},"regionId":{"value":"U1cjanVtYm9hcmdlbnRpbmFkMjM3"},"geoCoordinates":{"value":"-58.49954570000001,-34.6119862"},"addressId":{"value":"217306476"}}}', #'{"public":{"buyselectMethod":{"value":"recogida"},"regionId":{"value":"U1cjanVtYm9hcmdlbnRpbmFkMDM1"}}}',
     method: "POST",
     http2: true,
     fetch_type: "standard",
